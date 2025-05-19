14bab68f35353592ec142bd1529739dad443c41c
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2025-05-19 22:39:57 +0530

    Create data_batch3.py

---
+++ data_batch3.py
@@ -0,0 +1,10 @@
+# data_run1.py
+from run_batch import run_batch
+from helper import get_data_folder
+from main import main 
+import os
+
+folder = get_data_folder()
+files = sorted([f for f in os.listdir(folder) if f.endswith(".txt")])
+paths = [os.path.join(folder, f) for f in files[7:11]]
+run_batch(paths)
